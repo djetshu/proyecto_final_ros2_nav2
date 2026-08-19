27e549e762a02c8de8bb6501c78c6f9eac24aa6f
Daffer Queque <daffer.queque@ekumenlabs.com>
2026-08-19 15:11:47 -0500

    Fix SLAM mapping mode and remove duplicate AMCL/TF nodes
    Signed-off-by: Daffer Queque <daffer.queque@ekumenlabs.com>

--- bcr_bot/launch/nav2.launch.py
+++ bcr_bot/launch/nav2.launch.py
@@ -39,28 +39,12 @@ def generate_launch_description():
         ]
     )
     
-    amcl_node = Node(
-        package='nav2_amcl',
-        executable='amcl',
-        name='amcl',
-        output='screen',
-        parameters=[os.path.join(pkg_bcr, 'config', 'amcl_params.yaml')],
-    )
-
     map_server_node = Node(
         package='nav2_map_server',
         executable='map_server',
         name='map_server',
         output='screen',
         parameters=[{'yaml_filename': os.path.join(pkg_bcr, 'config', 'bcr_map.yaml')}],
-    )
-
-    static_transform_publisher_node = Node(
-        package='tf2_ros',
-        executable='static_transform_publisher',
-        name='map_to_odom',
-        output='screen',
-        arguments=['0', '0', '0', '0', '0', '0', 'map', 'odom']
     )
 
     remapper_node = Node(
@@ -74,9 +58,7 @@ def generate_launch_description():
 
     ld.add_action(nav2_launch_cmd)
     ld.add_action(rviz_launch_cmd)
-    ld.add_action(amcl_node)
     ld.add_action(map_server_node)
-    ld.add_action(static_transform_publisher_node)
     ld.add_action(remapper_node)
 
     return ld
